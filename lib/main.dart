1ef1171b8f9bb720add6738ee0bd57ea80c38719
gmorklla <gmorklla@gmail.com>
2019-06-05 16:31:20 -0500

    Segunda app mismo repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:app1/src/app.dart';
+import 'package:app1/src/pages/home_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Componentes App',
+        home: HomePage());
+  }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,53 +1,26 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final estiloTxt = new TextStyle(fontSize: 20.0, color: Colors.blueGrey);
-  final int conteo = 10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Título'),
-        centerTitle: true,
-        backgroundColor: Colors.teal,
-        elevation: 10.0,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Número de clicks:',
-              style: estiloTxt,
-            ),
-            Text('$conteo', style: estiloTxt)
-          ],
+        appBar: AppBar(
+          title: Text('Componentes'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.teal[400],
-        onPressed: () {
-          print('Add');
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('Business'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text('School'),
-          ),
-        ],
-        selectedItemColor: Colors.teal[800],
-      ),
+        body: _lista());
+  }
+
+  Widget _lista() {
+    return ListView(
+      children: _crearItems(),
     );
+  }
+
+  List<Widget> _crearItems() {
+    return [
+      ListTile(
+        title: Text('Hola mundo'),
+      )
+    ];
   }
 }
