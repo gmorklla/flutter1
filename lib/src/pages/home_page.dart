101d260f26aa9ac34ab7b12b638f45fd742cb8bb
gmorklla <gmorklla@gmail.com>
2019-06-09 23:57:02 -0500

    Added: Future builder para leer data en json y pintarla

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:app1/src/providers/menu_provider.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,16 +13,31 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    return ListView(
-      children: _crearItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _crearItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _crearItems() {
-    return [
-      ListTile(
-        title: Text('Hola mundo'),
-      )
-    ];
+  List<Widget> _crearItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final wTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(
+          Icons.ac_unit,
+          color: Colors.blueGrey,
+        ),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blueGrey),
+        onTap: () {},
+      );
+      opciones..add(wTemp)..add(Divider());
+    });
+    return opciones;
   }
 }
